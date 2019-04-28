8e72a53f1756725596c6c99e112ce83db85d042a
mightydhiyamatra <mightydhiyamatra@yahoo.co.id>
2019-04-28 11:10:33 +0700

    add server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+const app = express ();
+const port = process.env.PORT || 3000;
+const bodyParser = require ('body-parser');
+const routes = require ('./routes');
+
+app.use (
+  bodyParser.urlencoded ({
+    extended: true,
+  })
+);
+app.use (bodyParser.json ());
+
+routes (app);
+
+app.listen (port);
+console.log ('Express Started');
